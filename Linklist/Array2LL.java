a2c5f3c1025f5e6bf6fbf6b9f413d483e38be275
saatvik-10 <saatvikmadan529@gmail.com>
2024-11-11 16:54:31 +0100

    added array to LL

--- Linklist/Array2LL.java
+++ Linklist/Array2LL.java
@@ -20,7 +20,7 @@ public class Array2LL {
         Node head = new Node(arr[0]);
         Node mover = head;
         for (int i = 1; i < arr.length; i++) {
-            Node temp = new Node(arr[0]);
+            Node temp = new Node(arr[i]);
             mover.next = temp;
             mover = temp;
         }
